7de4bb90a816fdd50bb4804389bd3be5ba345ec6
sibbl08 <p.liebscher@ostfalia.de>
2025-11-12 14:47:49 +0100

    Add files via upload

---
+++ simulation.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+import mujoco
+import mujoco.viewer
+import time
+
+
+xml_path = (Path(__file__).parent / "Robot_V1" / "Robot_V1.xml").resolve()
+
+model = mujoco.MjModel.from_xml_path(str(xml_path))
+data = mujoco.MjData(model)
+dt = model.opt.timestep
+
+with mujoco.viewer.launch_passive(model, data) as viewer:
+    while viewer.is_running():
+        start = time.time()
+        mujoco.mj_step(model, data)
+        viewer.sync()
+        time.sleep(max(0, dt - (time.time() - start)))
